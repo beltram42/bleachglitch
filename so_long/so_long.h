10337186cd4d80151125b05b2acf02ef963058b5
Anthony LAMBERT <alambert@e1r5p4.42.fr>
2022-03-24 18:21:45 +0100

    debut so_long

---
+++ so_long/so_long.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alambert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 18:00:34 by alambert          #+#    #+#             */
+/*   Updated: 2022/03/24 18:09:20 by alambert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include "mlx_macos/mlx.h"
+#include "libft/libft.h"
+#include "get_next_line/get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+#endif
